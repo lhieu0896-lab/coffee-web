6eabdc83141ae967db9e8f44e993edfbe16a9cd1
lhieu0896-lab <lhieu0896@gmail.com>
2026-09-09 23:43:55 +0700

    Fix table selection screen and pin bar

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3534034966" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "338781884" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
